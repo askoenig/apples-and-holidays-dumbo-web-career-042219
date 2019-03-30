8e95d214b6aef51ae0739d921f7779a7efe4c7f2
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-30 21:06:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,6 +84,7 @@ barbeque_array = []
   holiday_hash.each do |season, holidays|
   
     holidays.each do |holiday, supplies|
+      binding.pry
       if supplies.include?("BBQ")
         barbeque_array << holiday
 
